bdd00abafe68cd0d0daf141da43a584ca6f998e4
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-10-28 17:55:27 +0100

    Spring boot init and postgres configuration

---
+++ delegation/src/main/java/pl/lodz/p/it/delegation/DelegationApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.delegation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelegationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelegationApplication.class, args);
+	}
+
+}
